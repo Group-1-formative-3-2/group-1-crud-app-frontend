20d0f8c21d14c6c11b618e80ade47f815f56a305
amycrampton <270066873@yoobeestudent.ac.nz>
2022-08-24 20:11:23 +1200

    update info showing/hiding

--- group-1-frontend/src/components/HomeView.vue
+++ group-1-frontend/src/components/HomeView.vue
@@ -1,105 +1,114 @@
 <template>
-<div>
-      <h2>Profile Count: {{profiles.length}}</h2>
-        <input type="text" v-model.trim="id">
-        <input type="text" placeholder="firstName" v-model="formValues.firstName">
-        <input type="text" placeholder="lastName" v-model="formValues.lastName">
-        <input type="text" placeholder="imageUrl" v-model="formValues.imageUrl">
-        <button @click="updateDoc">Update Document</button>
+    <div>
 
-      <ul>
-        <li v-for="profile in profiles" :key="profile">
-        <div>
-            <img :src="profile.imageURL" alt="">
-            {{profile._id}}
-            {{profile.firstName}}
-            {{profile.lastName}}
-            {{profile.About}}
-            {{profile.AuthorName}}
-            {{profile.Img}}
-
-            <a href="#" @click="getDoc(profile._id)">Edit</a> | 
-            <a href="#" @click="deleteDoc(profile._id)">Delete</a>
+        <div class="updatedInfo" v-show="updatedInfoShow">
+            <input type="text" v-model.trim="id" disabled placeholder="Work ID">
+            <input type="text" placeholder="About" v-model="formValues.About">
+            <input type="text" placeholder="Author Name" v-model="formValues.AuthorName">
+            <input type="text" placeholder="image Url" v-model="formValues.Img">
+            <button @click="updateDoc">Update Document</button>
         </div>
-        </li>
-      </ul>
-  </div>
+
+        <h2>Profile Count: {{profiles.length}}</h2>
+
+        <ul>
+            <li v-for="profile in profiles" :key="profile">
+                <div>
+                    <img :src="profile.Img" alt="">
+                    {{profile._id}}
+                    {{profile.About}}
+                    {{profile.AuthorName}}
+
+                    <a href="#" @click="getDoc(profile._id), updatedInfoShow = true" :class="{active: updatedInfoShow == true }">Edit</a> | 
+                    <a href="#" @click="deleteDoc(profile._id)">Delete</a>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-const api = "https://fabulous-cocada-f68050.netlify.app/.netlify/functions/api/";
-  export default{
-    name: 'HomeView',
-    data() {
-      return {
-        profiles: [],
-        id: "",
-        formValues: {
-          About: "",
-          AuthorName: "",
-          Img: "",
+    const api = "https://fabulous-cocada-f68050.netlify.app/.netlify/functions/api/";
+    export default{
+        name: 'HomeView',
+        data() {
+        return {
+            profiles: [],
+            id: "",
+            formValues: {
+            About: "",
+            AuthorName: "",
+            Img: "",
+            updatedInfoShow: false
+            }
         }
-      }
-    },
-    methods: {
-      getDoc(id) { 
-        this.id = id
-        fetch(api + this.id, {
-            method: 'GET'
-          })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data)
-            this.getAll();
-          })
-          .catch((err) => {
-            if (err) throw err;
-          })
-      },
-      updateDoc(){
-        fetch(api + this.id, {
-          method: 'PUT',
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(this.formValues)
-        })
-        .then((response) => response.text())
-        .then((data) => {
-          console.log(data)
-           this.getAll();
-        })
-        .catch((err) => {
-          if (err) throw err;
-        })
-      },
-      deleteDoc(id) { 
-        fetch(api + id, {
-            method: 'DELETE'           
-          })
-          .then((response) => response.text())
-          .then((data) => {
-            console.log(data)
-            this.getAll();
-          })
-          .catch((err) => {
-            if (err) throw err;
-          })
-        
-      },
-      getAll() {
-            fetch(api)
-                .then((response) => response.json())
-                .then((data) => {
-                this.profiles = data
-                })
-                .catch((err) => {
+        },
+        methods: {
+            // updateShow(){
+            //     this.updatedInfoShow = true
+            // },
+        getDoc(id) { 
+            this.id = id
+            fetch(api + this.id, {
+                method: 'GET'
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                this.getAll();
+            })
+            .catch((err) => {
                 if (err) throw err;
-                })
-      }
-    },
-    mounted(){
-      this.getAll();
+            })
+        },
+        updateDoc(){
+            fetch(api + this.id, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.formValues)
+            })
+            .then((response) => response.text())
+            .then((data) => {
+            console.log(data)
+            alert("Work has been Updated")
+            this.getAll()
+            this.formValues = ""
+            this.updatedInfoShow = false
+            })
+            .catch((err) => {
+            if (err) throw err;
+            })
+        },
+        deleteDoc(id) { 
+            fetch(api + id, {
+                method: 'DELETE'           
+            })
+            .then((response) => response.text())
+            .then((data) => {
+                console.log(data)
+                alert("Work has been Deleted")
+                this.getAll();
+            })
+            .catch((err) => {
+                if (err) throw err;
+            })
+            
+        },
+        getAll() {
+            fetch(api)
+            .then((response) => response.json())
+            .then((data) => {
+            this.profiles = data
+            })
+            .catch((err) => {
+            if (err) throw err;
+            })
+        }
+        },
+        mounted(){
+        this.getAll();
+        }
     }
-  }
 </script>
