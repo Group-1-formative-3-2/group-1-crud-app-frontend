222e2b73a5e06058f1b4783222d56d5ece812b4c
KayZ <270159822@yoobeestudent.ac.nz>
2022-08-25 14:04:07 +1200

    Update AboutView.vue

--- group-1-frontend/src/components/AboutView.vue
+++ group-1-frontend/src/components/AboutView.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-        <h1>About Page</h1>
+    <div  class="about-body">
+        <div class="about-container">
+            <div class="about">
+
+            <p>Lorem ipsum dolor sit amet. Et minus sint qui sequi quia ad deserunt officia. Qui voluptates aperiam est perspiciatis fugiat a repudiandae explicabo non enim illo. </p> 
+            <br>
+            <p>Eum quae soluta et adipisci molestiae et obcaecati galisum et doloremque ipsum non itaque ipsam At quis magni. Quo quia voluptatem sed quod voluptates ut autem fugiat aut quia nihil et eveniet laudantium est blanditiis libero. </p>
+            <br>
+            <p>Et neque animi id officiis asperiores et quaerat laboriosam qui debitis delectus non architecto eius. In ratione beatae non pariatur fuga et praesentium similique. </p>
+            <br>
+            <p>Ea maxime expedita et ullam impedit aut soluta consequatur ut praesentium architecto. Id iste quaerat id iste esse qui commodi itaque est expedita nesciunt. Est commodi minima sed sunt ullam non voluptatum officia ex laborum voluptas qui exercitationem impedit  tempore dolor. Et voluptas eligendi qui earum autem et consequatur nesciunt qui consequatur distinctio non molestiae quia ut omnis sapiente. </p>
+       
+
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,3 +21,42 @@
     export default{
         name: 'AboutView'}
 </script>
+
+
+<style>
+.about-body {
+    text-align: center;
+    justify-content: center;
+}
+
+.about-container {
+    border-radius: 30px;
+    width: 90vw;
+    padding: 20px;
+    margin: 40px;
+    background-color: var(--background);
+  
+}
+
+.about {
+    background-color: white;
+    border-radius: 30px;
+    margin: 50px;
+    padding: 50px;
+    overflow-y: scroll;
+    box-shadow: 10px 10px 10px #9b9b9b;
+    text-align: left;
+
+}
+
+#about p {
+    font-size: 22px;
+    font-family: 'Montserrat', sans-serif;
+    color: black;
+    opacity: 100%;
+    text-align: left;    
+}
+
+
+
+</style>
