028eeae277039796c00a86ea55267953f34ffd71
kristkabarrington <270048954@yoobeestudent.ac.nz>
2022-08-26 13:11:14 +1200

    about_update

--- group-1-frontend/src/components/AboutView.vue
+++ group-1-frontend/src/components/AboutView.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="about">
-        <h1>About Page</h1>
+         <section id="body">
+    <section id="container">
+        <div id="about">
+
+            <p>Lorem ipsum dolor sit amet. Et minus sint qui sequi quia ad deserunt officia. Qui voluptates aperiam est perspiciatis fugiat a repudiandae explicabo non enim illo. </p> 
+            <br>
+            <p>Eum quae soluta et adipisci molestiae et obcaecati galisum et doloremque ipsum non itaque ipsam At quis magni. Quo quia voluptatem sed quod voluptates ut autem fugiat aut quia nihil et eveniet laudantium est blanditiis libero. </p>
+            <br>
+            <p>Et neque animi id officiis asperiores et quaerat laboriosam qui debitis delectus non architecto eius. In ratione beatae non pariatur fuga et praesentium similique. </p>
+            <br>
+            <p>Ea maxime expedita et ullam impedit aut soluta consequatur ut praesentium architecto. Id iste quaerat id iste esse qui commodi itaque est expedita nesciunt. Est commodi minima sed sunt ullam non voluptatum officia ex laborum voluptas qui exercitationem impedit  tempore dolor. Et voluptas eligendi qui earum autem et consequatur nesciunt qui consequatur distinctio non molestiae quia ut omnis sapiente. </p>
+            <br>
+            <p>A nostrum galisum ut aliquam amet id quia nihil et eveniet recusandae non quibusdam rerum cum internos quis ut commodi doloremque. Ut eius cumque nam soluta quia ad asperiores dolore. </p>
+
+        </div>
+
+    </section>
+</section>
     </div>
 </template>
 
@@ -8,3 +25,38 @@
     export default{
         name: 'AboutView'}
 </script>
+
+<style scoped>
+#body {
+    text-align: center;
+    justify-content: center;
+}
+
+#container {
+    border-radius: 30px;
+    background-color:#e3dceb;
+    width: 90vw;
+    padding: 20px;
+    margin: 40px;
+  
+}
+
+#about {
+    background-color: white;
+    border-radius: 30px;
+    margin: 50px;
+    padding: 50px;
+    overflow-y: scroll;
+    box-shadow: 10px 10px 10px #9b9b9b;
+
+
+}
+
+#about p {
+    font-family: 'Montserrat', sans-serif;
+    color: black;
+    opacity: 100%;
+    
+
+    
+}</style>
